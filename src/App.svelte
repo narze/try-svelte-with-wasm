15036fb248ff81ca3c815653709161f5b8c3cd31
Manassarn Manoonchai <manassarn@gmail.com>
2022-11-22 00:29:04 +0700

    Apply Photon resize & filter

--- src/App.svelte
+++ src/App.svelte
@@ -1,64 +1,84 @@
 <script lang="ts">
-  import svelteLogo from "./assets/svelte.svg";
-  import Counter from "./lib/Counter.svelte";
+  import svelteLogo from "./assets/svelte.svg"
+  import * as photon from "@silvia-odwyer/photon"
+  import { onMount } from "svelte"
 
-  let img;
-  let canvas;
-  let photon;
+  let img: HTMLImageElement
+  let canvas: HTMLCanvasElement
+  let resizedCanvas: HTMLCanvasElement
+  let fileInput: HTMLInputElement
 
-  import("@silvia-odwyer/photon").then((importedPhoton) => {
-    photon = importedPhoton;
-  });
-
-  function filterImage() {
+  function filterImage(img: HTMLImageElement) {
     // Create a canvas and get a 2D context from the canvas
-    let ctx = canvas.getContext("2d");
+    let ctx = canvas.getContext("2d")
+    canvas.width = img.width
+    canvas.height = img.height
 
     // Draw the image element onto the canvas
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(img, 100 * Math.random(), 0);
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.drawImage(img, 0, 0)
 
     // Convert the ImageData found in the canvas to a PhotonImage (so that it can communicate with the core Rust library)
-    let image = photon.open_image(canvas, ctx);
+    let image = photon.open_image(canvas, ctx)
+
+    let w = image.get_width() * 2
+    let h = image.get_height() * 2
 
     // Filter the image, the PhotonImage's raw pixels are modified
-    photon.filter(image, "radio");
+    photon.filter(image, "rosetint")
+
+    let resultCanvas = photon.resize_img_browser(
+      image,
+      w,
+      h,
+      photon.SamplingFilter.Nearest
+    )
+    let resultCtx = resultCanvas.getContext("2d")
+    let imgdata = resultCtx.getImageData(0, 0, w, h)
+
+    let resizedCtx = resizedCanvas.getContext("2d")
+    resizedCanvas.width = imgdata.width
+    resizedCanvas.height = imgdata.height
+
+    resizedCtx.clearRect(0, 0, resizedCanvas.width, resizedCanvas.height)
+    resizedCtx.putImageData(imgdata, 0, 0)
 
     // Place the modified image back on the canvas
-    photon.putImageData(canvas, ctx, image);
+    // photon.putImageData(canvas, ctx, newimage)
   }
+
+  onMount(() => {
+    fileInput.onchange = function () {
+      let file = fileInput.files[0]
+      let reader = new FileReader()
+      reader.onload = function (e) {
+        const image = new Image()
+        image.src = e.target.result as string
+
+        image.onload = () => {
+          filterImage(image)
+        }
+      }
+      reader.readAsDataURL(file)
+    }
+  })
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img
-        bind:this={img}
-        src={svelteLogo}
-        class="logo svelte"
-        alt="Svelte Logo"
+  <h1>Try Svelte with Wasm</h1>
+
+  <center>
+    <div><input bind:this={fileInput} type="file" /></div>
+
+    <div>Loaded image<canvas id="canvas" bind:this={canvas} /></div>
+    <hr />
+    <div>
+      Resized+filtered image<canvas
+        id="resized-canvas"
+        bind:this={resizedCanvas}
       />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <button on:click={filterImage}>Click</button>
-    <canvas id="canvas" bind:this={canvas} />
-  </div>
-
-  <p>
-    Check out <a
-      href="https://github.com/sveltejs/kit#readme"
-      target="_blank"
-      rel="noreferrer">SvelteKit</a
-    >, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
+    </div>
+  </center>
 </main>
 
 <style>
@@ -72,8 +92,5 @@
   }
   .logo.svelte:hover {
     filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
   }
 </style>
